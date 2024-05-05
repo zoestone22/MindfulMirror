5fe07d576fd65dbd157e352ce89735d701afa939
Zoe <67975891+zoestone22@users.noreply.github.com>
2024-05-05 17:38:30 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,11 @@
+modules: [
 
+    //this adds the affirmation module to the modules array
+    {
+        module:"affirmations",
+        position: "lower_third", //this positions where the affirmation is displayed
+        
+    }
+
+
+]
